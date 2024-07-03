ed615cff31576249738e5fa56b35b06dc852756c
firecraftgaming <eliyah@sundstroem.com>
2024-07-03 17:04:09 +0200

    chore: fixed config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,7 @@
 export const config = {
-    input: '/Users/eliyahsundstrom/Downloads/input',
-    output: '/Users/eliyahsundstrom/Downloads/output',
+    // input: '/Users/eliyahsundstrom/Downloads/input',
+    // output: '/Users/eliyahsundstrom/Downloads/output',
+
+    input: 'C:\\Users\\TiNkiN\\Documents\\lappis\\video-encoder\\input',
+    output: 'C:\\Users\\TiNkiN\\Documents\\lappis\\video-encoder\\output',
 };
